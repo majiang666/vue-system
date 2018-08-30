9f2a9d3cefa9cab8e8742dfb8896faa738a79273
马江 <497442190@qq.com>
2018-08-30 15:25:46 +0800

    初始化项目

---
+++ system-admin/src/components/module/img/img.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="img-mode">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pic",
+  data:function(){ 
+    return {
+      msg:"图片模板"
+    }
+    
+  },
+  components:{
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .img-mode{
+    margin-top:60px;
+    margin-left:200px;
+    overflow-y: auto;
+    padding:20px;
+  }
+</style>
