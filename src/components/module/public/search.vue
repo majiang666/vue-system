934462499b534fb7472313fd9bb0539ddb72242c
马江 <497442190@qq.com>
2018-08-31 11:09:45 +0800

    初始化项目

---
+++ src/components/module/public/search.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="search">
+    <select name="" id="" v-model="searchClassify">
+      <option value="0">全部分类</option>
+      <option value="1">分类1</option>
+      <option value="2">分类2</option>
+    </select>
+    搜索内容：
+    <input type="text" v-model="searchTxt">
+    <button @click="searchBtn">搜索</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "search",
+  data:function(){ 
+    return {
+      searchTxt:"",
+      searchClassify:"0"
+    } 
+  },
+  methods:{
+    searchBtn(){
+      let data = {};
+      data.searchTxt = this.searchTxt;
+      data.searchClassify = this.searchClassify;
+      this.$emit("showInfo",data)
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .search select{
+    width:100px;
+    height:26px;
+    border:solid 1px #dedede;
+  }
+  .search input{
+    width:150px;
+    height:26px;
+    border:solid 1px #dedede;
+    margin:0 10px;
+  }
+  .search button{
+    width:60px;
+    height:26px;
+    background: #42a6ef;
+    border:0;
+    color:#fff;
+    line-height: 26px;
+    vertical-align: middle;
+  }
+</style>
---
+++ src/components/module/public/tips.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="tips">
+  {{title}}
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "tips",
+  props:["title"],
+  data:function(){ 
+    return {
+      
+    }
+  },
+  methods:{
+   
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .tips{
+    position: fixed;
+    height:35px;
+    line-height: 35px;
+    left:0;
+    right:0;
+    bottom:10%;
+    background: rgba(0, 0, 0, 0.5);
+    z-index:9999;
+    text-align: center;
+    color:#fff;
+    width:120px;
+    margin:0 auto;
+    border-radius: 20px;
+  }
+  
+</style>
