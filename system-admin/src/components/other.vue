3182ca74b0afb1bf2633de93ac7dbee7c86026eb
马江 <497442190@qq.com>
2018-10-17 17:02:47 +0800

    解决冲突

---
+++ system-admin/src/components/other.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="other">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return {
+            msg:"11111111111111"
+        }
+    },
+  mounted:function(){
+    //   this.$nextTick( () => {
+   this.bus.$on("test",msg => {
+       this.msg = msg;
+   });
+    //   });
+  }
+}
+</script>
+
+<style>
+
+</style>
+
